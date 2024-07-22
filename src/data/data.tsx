a8aa57fb7281c30c664ebf6de821d0e14828fda9
YingyGong <ygong@caltech.edu>
2024-07-22 14:18:25 -0700

    edited title

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -34,8 +34,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Yingying Gong Resume',
+  description: "Personal Website built with Tim Baker's react resume template",
 };
 
 /**
