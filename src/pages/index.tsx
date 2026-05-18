1ad090a24bd5305c0530f8dfe9feb7f581318b61
YingyGong <ygong@caltech.edu>
2026-05-17 20:07:18 -0700

    disable blog for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
-import Blog from '../components/Sections/Blog';
+// import Blog from '../components/Sections/Blog';
 import Resume from '../components/Sections/Resume';
 // import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
@@ -22,7 +22,7 @@ const Home: FC = memo(() => {
       <Header />
       <Hero />
       <About />
-      <Blog />
+      {/* <Blog /> */}
       <Resume />
       <Portfolio />
       {/* <Testimonials /> */}
